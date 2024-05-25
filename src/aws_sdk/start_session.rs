b6067c5a97a8bf62405a0ed09b7155736aab30dd
kv5h <107398371+kv5h@users.noreply.github.com>
2024-05-25 19:04:23 +0900

    WIP Update

--- src/aws_sdk/start_session.rs
+++ src/aws_sdk/start_session.rs
@@ -1,11 +1,13 @@
+use std::{io, ops::Deref, process::Stdio};
+
 use aws_config::BehaviorVersion;
 use aws_types::region::Region;
-use futures_util::{future, pin_mut, StreamExt}; // TODO:
-use tokio::io::{AsyncReadExt, AsyncWriteExt}; // TODO:
-use tokio::sync::mpsc;
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message}; // TODO: // TODO:
+use serde_json::json;
+use tokio::process::Command;
 
-#[derive(PartialEq, Debug)]
+const SESSION_MANAGER_BIN_NAME: &str = "session-manager-plugin";
+
+#[derive(PartialEq, Debug, Clone, Copy)]
 enum DocumentName {
     AwsStartPortForwardingSession,
     AwsStartPortForwardingSessionToRemoteHost,
@@ -66,7 +68,27 @@ async fn get_client(region: &str) -> aws_sdk_ssm::Client {
     aws_sdk_ssm::Client::new(&config)
 }
 
-pub async fn start_session(prop: &SessionManagerProp) -> Result<(), aws_sdk_ssm::Error> {
+fn check_binary_exist() {
+    match subprocess::Exec::cmd(SESSION_MANAGER_BIN_NAME).join() {
+        Ok(o) => {
+            assert!(o.success())
+        },
+        Err(e) => {
+            log::error!("{}. The executable {} not found. Install it at {}",
+                e,
+                SESSION_MANAGER_BIN_NAME,
+                "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
+            );
+            panic!()
+        },
+    }
+}
+
+/// Reference: https://github.com/aws/session-manager-plugin/blob/mainline/src/sessionmanagerplugin/session/session.go
+pub async fn start_session(prop: &SessionManagerProp) -> Result<(), Box<dyn std::error::Error>> {
+    // Assert executable exist
+    check_binary_exist();
+
     let mode = if prop.remote_host.is_some() {
         DocumentName::AwsStartPortForwardingSessionToRemoteHost
     } else if prop.local_port.is_some() && prop.remote_port.is_some() {
@@ -84,14 +106,14 @@ pub async fn start_session(prop: &SessionManagerProp) -> Result<(), aws_sdk_ssm:
                 .start_session()
                 .target(&prop.instance_id)
                 .document_name(m.get_document_name().unwrap())
-                .parameters("host", vec![prop.remote_host.clone().unwrap().to_string()])
+                .parameters("host", vec![prop.remote_host.clone().unwrap().to_string()]) // TODO:
                 .parameters("portNumber", vec![prop.remote_port.unwrap().to_string()])
                 .parameters("localPortNumber", vec![prop
                     .local_port
                     .unwrap()
                     .to_string()])
                 .send()
-                .await?
+                .await
         },
         m if m == DocumentName::AwsStartPortForwardingSession => {
             get_client(&prop.region)
@@ -105,7 +127,7 @@ pub async fn start_session(prop: &SessionManagerProp) -> Result<(), aws_sdk_ssm:
                     .unwrap()
                     .to_string()])
                 .send()
-                .await?
+                .await
         },
         _ => {
             get_client(&prop.region)
@@ -113,52 +135,124 @@ pub async fn start_session(prop: &SessionManagerProp) -> Result<(), aws_sdk_ssm:
                 .start_session()
                 .target(&prop.instance_id)
                 .send()
-                .await?
+                .await
         },
     };
 
-    log::info!("Session ID: {}", resp.session_id().unwrap());
-    log::info!("Stream URL: {}", resp.stream_url().unwrap()); // TODO:
-    log::info!("Token value: {}", resp.token_value().unwrap()); // TODO:
+    let resp_json: serde_json::Value;
+    match resp {
+        Ok(o) => {
+            log::info!("Session ID: {}", o.session_id().unwrap());
+            resp_json = json!({
+                // TODO: NG
+                //"session_id": o.session_id().unwrap(),
+                //"token_value": o.token_value().unwrap(),
+                //"stream_url": o.token_value().unwrap(),
+                "SessionId": o.session_id().unwrap(),
+                "TokenValue": o.token_value().unwrap(),
+                "StreamUrl": o.stream_url().unwrap(),
+            });
+        },
+        Err(e) => {
+            log::error!("{e}");
+            return Err(e.into());
+        },
+    }
 
-    let connect_addr = resp.stream_url().unwrap();
-    let url = url::Url::parse(&connect_addr).unwrap();
-    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    tokio::spawn(read_stdin(stdin_tx));
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-    println!("WebSocket handshake has been successfully completed");
-
-    let (write, read) = ws_stream.split();
-
-    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let data = message.unwrap().into_data();
-            tokio::io::stdout().write_all(&data).await.unwrap();
-        })
+    let session_manager_param = match mode {
+        m if m == DocumentName::AwsStartPortForwardingSessionToRemoteHost => {
+            json!({
+                "Target" : &prop.instance_id,
+                "DocumentName": m.get_document_name().unwrap(),
+                "parameters": {
+                    "host": vec![prop.remote_host.clone().unwrap().to_string()],
+                    "portNumber": vec![prop.remote_port.unwrap().to_string()],
+                    "localPortNumber": vec![prop.local_port.unwrap().to_string()]
+                }
+            })
+        },
+        m if m == DocumentName::AwsStartPortForwardingSession => {
+            json!({
+                "Target" : &prop.instance_id,
+                "DocumentName": m.get_document_name().unwrap(),
+                "parameters": {
+                    "portNumber": vec![prop.remote_port.unwrap().to_string()],
+                    "localPortNumber": vec![prop.local_port.unwrap().to_string()]
+                }
+            })
+        },
+        _ => {
+            json!({"Target" : &prop.instance_id})
+        },
     };
 
-    pin_mut!(stdin_to_ws, ws_to_stdout);
-    future::select(stdin_to_ws, ws_to_stdout).await;
+    println!("{}", session_manager_param.to_string()); // TODO:
+    println!(
+        "{} {} {} StartSession '' {} {}",
+        SESSION_MANAGER_BIN_NAME,
+        resp_json.to_string(),
+        prop.region,
+        session_manager_param.to_string(),
+        format!("https://ssm.{}.amazonaws.com", prop.region)
+    );
+
+    // Spawning Subprocess
+    tokio::process::Command::new(SESSION_MANAGER_BIN_NAME)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .arg(resp_json.to_string())
+        .arg(&prop.region)
+        .arg("StartSession")
+        .arg("")
+        .arg(session_manager_param.to_string())
+        .arg(format!("https://ssm.{}.amazonaws.com", prop.region))
+        .spawn()?;
+
+    //  subprocess ver.
+    // let command = [
+    // SESSION_MANAGER_BIN_NAME,
+    // &resp_json.to_string(),
+    // &prop.region,
+    // "StartSession",
+    // "",
+    // &session_manager_param.to_string(),
+    // &format!("https://ssm.{}.amazonaws.com", prop.region),
+    // ];
+    // let mut p = subprocess::Popen::create(&command, subprocess::PopenConfig {
+    //     // stdin: subprocess::Redirection::Pipe,
+    //     stdout: subprocess::Redirection::Pipe,
+    //     // stdout: subprocess::Redirection::Merge,
+    //     stdin: subprocess::Redirection::Pipe,
+    //     ..Default::default()
+    // })?;
+    //
+    // let mut p = subprocess::Popen::create(&command, subprocess::PopenConfig::default())?;
+    //
+    // Since we requested stdout to be redirected to a pipe, the parent's
+    // end of the pipe is available as p.stdout.  It can either be read
+    // directly, or processed using the communicate() method:
+    // let (out, err) = p.communicate(None)?;
+    // let _ = p.wait()?;
+    //
+    // check if the process is still alive
+    // if let Some(_) = p.poll() {
+    // the process has finished
+    // } else {
+    // it is still running, terminate it
+    // p.terminate()?;
+    // }
+    //
+    // let exit_status = subprocess::Exec::cmd(SESSION_MANAGER_BIN_NAME)
+    //    .arg(resp_json.to_string())
+    //    .arg(&prop.region)
+    //    .arg("StartSession")
+    //    .arg("")
+    //    .arg(session_manager_param.to_string())
+    //    .arg(format!("https://ssm.{}.amazonaws.com", prop.region))
+    //    .join()?;
+    // assert!(exit_status.success());
 
     Ok(())
-    // let resp = get_client(&prop.region).await.start_session().document_name(input)
-}
-
-// TODO:
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-    loop {
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n,
-        };
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
-    }
 }
 
 // TODO: test
